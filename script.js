7804db331b7be978dfe046b3673fdb9e0cf26276
Subrat Shrestha <60907777+SubratShrestha@users.noreply.github.com>
2020-02-14 14:02:46 +1100

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         //2. Display the result
         document.getElementById('dice-1').style.display = 'block';
         document.getElementById('dice-2').style.display = 'block';
-        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
-        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+        document.getElementById('dice-1').src = 'dice/dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice/dice-' + dice2 + '.png';
 
         //3. Update the round score IF the rolled number was NOT a 1
         if (dice1 !== 1 && dice2 !== 1) {
